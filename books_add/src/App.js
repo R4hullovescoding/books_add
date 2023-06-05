5faf71433e8b2f7a7007489a36923549bd22464e
Rahul <r2h4l143@gmail.com>
2023-06-06 01:07:15 +0530

    createBook function add

--- books_add/src/App.js
+++ books_add/src/App.js
@@ -1,6 +1,12 @@
+import {useState} from "react";
 function App(){
+    const [books,setBooks]=useState();
+    const onCreateBook=({title})=>{
+        const updatebook=[{id:(Math.floor(Math.random()*99)),title:title}];
+        setBooks(updatebook);
+    };
     return (<div>
-        Welcome
+       <createBook createBook={onCreateBook}/>
     </div>);
 }
 export default App;
