ac4cc1ad361abe103d4bef52e431d9af4a57fa87
Rahul <r2h4l143@gmail.com>
2023-06-06 19:30:35 +0530

    App Changed and CreateBook Created and completed

--- books_add/src/App.js
+++ books_add/src/App.js
@@ -1,6 +1,32 @@
+import {useState} from "react";
+import CreateBooks from "./CreateBooks";
 function App(){
+    const [books,setbooks]=useState([]);
+    const onCreateBook=(title)=>{
+        const updateBooks=[...books,{id:(Math.floor(Math.random()*999)),title:title}];
+        setbooks(updateBooks);
+    };
+    // const handleDelete=async(id)=>{
+    //     const deletereq=await axios.delete(`http://localhost:3005/books/${id}`);
+    //     const updatebooks=books.filter((book,index)=>{
+    //         return book.id!==id;
+    //     });
+    //     setbook(updatebooks);
+    // };
+    // const handleEdit=async (id,newtitle)=>{
+    //     const editrq=await axios.put(`http://localhost:3005/books/${id}`,{
+    //         title:newtitle,
+    //     });
+    //     const updatebook=books.map((book,index)=>{
+    //         if(book.id===id){
+    //             return {...books,...editrq.data};
+    //         }
+    //         return book;
+    //     });
+    //     setbook(updatebook);
+    // };
     return (<div>
-        Welcome
+        <CreateBooks onCreate={onCreateBook} />
     </div>);
 }
 export default App;
---
+++ books_add/src/CreateBooks.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+function CreateBooks({onCreate}) {
+    const [title,settitle]=useState("");
+    const handleChange=(event)=>{
+        settitle(event.target.value);
+    }
+    const handleFormSubmit=(event)=>{
+        event.preventDefault();
+        onCreate(title);
+        settitle("");
+    }
+    return (
+        <div className="createBookMain">
+        <h3>Add BooK</h3>
+        <form onSubmit={handleFormSubmit} className="createForm">
+            <label>Title : </label>
+            <input value={title} onChange={handleChange} type="text"/>
+        <button className="createBtn">Submit</button>
+        </form>
+    </div>
+    );
+}
+export default CreateBooks;
